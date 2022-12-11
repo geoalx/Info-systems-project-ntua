812713b7e09451a0e4891bc4da63f0a1db54d287
lymperop <jimlibo13@gmail.com>
2022-12-11 14:29:21 +0200

    added some comments in the code

--- data_generate.py
+++ data_generate.py
@@ -7,7 +7,11 @@ from confluent_kafka import Producer
 F = Faker()
 
 class Sensor:
-
+    '''
+    Base class for Sensors. Each object corresponds to one sensor, and has a name and a value_range.
+    By default, interval is 15 minutes, and late is false. The late parameter indicates whether or not
+    to generate late-event-data.
+    '''
     def __init__(self, name, val_range, late=False):
         self.name = name
         self.late=late
@@ -15,20 +19,20 @@ class Sensor:
         self.val_range = val_range
         self.value = float(F.random.randrange(self.val_range[0]*100, self.val_range[1]*100)/100)
         self.time = datetime.now()
-        self.late_count = 0
+        self.late_count = 0    # used to find when to send late-event-data
 
     def generate(self):
         to_print = "{} | {}".format(self.time.strftime("%Y-%m-%d %H:%M"),round(self.value,2))
         self.time += self.interval
         self.value = float(F.random.randrange(self.val_range[0]*100, self.val_range[1]*100)/100)
 
-        if self.late_count % 20 == 0 and self.late_count != 0 and self.late:
+        if self.late_count % 20 == 0 and self.late_count != 0 and self.late:   # add late event from 2 days before
             value_2 = float(F.random.randrange(self.val_range[0]*100, self.val_range[1]*100)/100)
             late_time = self.time - timedelta(days=2)
             to_print_2 = "{} | {}".format(late_time.strftime("%Y-%m-%d %H:%M"),round(value_2,2))
             to_print += '$' + to_print_2
 
-        if self.late_count % 120 == 0 and self.late_count != 0  and self.late:
+        if self.late_count % 120 == 0 and self.late_count != 0  and self.late:  # add late event from 10 days before
             value_10 = float(F.random.randrange(self.val_range[0]*100, self.val_range[1]*100)/100)
             late_time = self.time - timedelta(days=10)
             to_print_10 = "{} | {}".format(late_time.strftime("%Y-%m-%d %H:%M"),round(value_10,2))   
@@ -40,6 +44,12 @@ class Sensor:
 
 
 class SumSensor(Sensor):
+    '''
+    Sub-class of the previous Sensor class. It represents the sensors that measure cumulative data on a daily
+    basis. The difference from the parent class is that the start with a value of 0 and the interval is 1 day
+    instead of 15 minutes. Also, the generate function returns the previous value plus a random amount, while 
+    in the Sensor class it returns a random value from val_range.
+    '''
 
     def __init__(self, name, val_range):
         super().__init__(name, val_range)
@@ -53,22 +63,25 @@ class SumSensor(Sensor):
         self.time += self.interval
         return to_print
 
-class MoveSensor(Sensor):
 
+class MoveSensor(Sensor):
+    '''
+    This class represents the moving sensor, which simply sends 1 if it detected motion in random times.
+    '''
     def __init__(self, name):
         self.name = name
-        self.count = 0
+        self.count = 0    # used to determine when to send 1
         self.time = datetime.now()
-        self.ace_index = F.random.sample(range(0,96),4)
+        self.ace_index = F.random.sample(range(0,96),4)   # choose 4 random times to send 1
 
     def generate(self):
         if(self.count in self.ace_index):
             # self.time += timedelta(minutes=self.count*15)
             to_print = "{} | {}".format((self.time + timedelta(minutes=self.count*15)).strftime("%Y-%m-%d %H:%M"),1)
         else:
-            to_print = "NO DATA"
+            to_print = "NO DATA"   # could also be None, will be filtered in next layer
 
-        if self.count == 95:
+        if self.count == 95:   # if one day passed, reset the counter and pick new random times to send 1
             self.count, self.time = 0, self.time + timedelta(days=1)
             self.ace_index = F.random.sample(range(0,96),4)
 
@@ -85,6 +98,7 @@ if __name__ == "__main__":
     
     headers_to_test = ["TH1","HVAC1","Etot","W1"]
     
+    # Producer Configuration
     conf = {'bootstrap.servers': 'pkc-lz6r3.northeurope.azure.confluent.cloud:9092',
         'sasl.mechanism' : 'PLAIN',
         'security.protocol': 'SASL_SSL',
@@ -92,7 +106,7 @@ if __name__ == "__main__":
         'sasl.password' : 'iPaie4dfn3uUM/NhmHGnMlv2mIcOr+wIBIO9xuLJgBf+VZaZFldaBkcexvEpRXpK'
     }
 
-
+    # connect with the kafka broker as a producer
     producer = Producer(conf)
 
     producer.flush()
@@ -106,6 +120,7 @@ if __name__ == "__main__":
                 f.write(temp + "\n")
 
                 if(sensor.name in headers_to_test):
-                    for t in temp.split("$"):
-                        producer.produce(topic=sensor.name, value=str(t))
+                    for t in temp.split("$"):  # split in $ to find posible late events
+                        # send the generated data to the appropriate kafka broker topic (channel)
+                        producer.produce(topic=sensor.name, value=str(t))  
                         producer.flush()
