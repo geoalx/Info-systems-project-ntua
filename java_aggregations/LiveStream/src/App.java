6674e8b3f07882f2072ebea5da89661e0f59864c
lymperop <jimlibo13@gmail.com>
2023-01-03 20:17:49 +0200

    debug attemp 1

--- java_aggregations/LiveStream/src/App.java
+++ java_aggregations/LiveStream/src/App.java
@@ -10,30 +10,30 @@
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.ForeachAction;
 
 import java.util.Properties;
 
 public class App {
     public static void main(String[] args) {
-        Properties props = new Properties();
+Properties props = new Properties();
         // set the stream configurations
-        //props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-example");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-example");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 
         StreamsBuilder builder = new StreamsBuilder();
-        // define the processing topology here
-
-        //subscribe to the topic TH1 and print the message
-        builder.stream("TH1").foreach((key, value) -> System.out.println("key: " + key + " value: " + value));
-
-        // build the topology and start streaming
+        // define the input stream and subscribe to it
+        KStream<String, String> inputStream = builder.stream("TH1");
+        inputStream.foreach(new ForeachAction<String, String>() {
+            @Override
+            public void apply(String key, String value) {
+                System.out.println("Received message: key = " + key + ", value = " + value);
+            }
+        });
 
         KafkaStreams streams = new KafkaStreams(builder.build(), props);
         streams.start();
-
-
-        // KafkaStreams streams = new KafkaStreams(builder.build(), props);
-        // streams.start();
 
         // shutdown hook to correctly close the streams application
         Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
