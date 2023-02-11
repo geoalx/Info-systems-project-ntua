894a154f5ee0e29ecbbee773d9869fcd7845b44f
lymperop <jimlibo13@gmail.com>
2023-02-11 21:41:13 +0200

    change DateParser due to possible bugs

--- java_aggregations/LiveStream/src/CustomTimestampExtractor.java
+++ java_aggregations/LiveStream/src/CustomTimestampExtractor.java
@@ -1,19 +1,27 @@
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.streams.processor.TimestampExtractor;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Date;
+import java.util.Locale;
+import java.sql.Timestamp;
 
 public class CustomTimestampExtractor implements TimestampExtractor {
     @Override
     public long extract(ConsumerRecord<Object, Object> record, long previousTimestamp) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("YYYY-MM-dd HH:mm");
-        try{
-            Date parseDate = dateFormat.parse(record.value().toString().split("\\|")[0]);
-            return (parseDate.getTime());
+
+        String date = record.value().toString().split("\\|")[0];
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm", Locale.ENGLISH);
+        LocalDateTime newdate = null;
+        try {
+            newdate = LocalDateTime.parse(date, dtf);
+        } catch (DateTimeParseException e) {
+            System.out.print("Error occured during date parsing");
         }
-        catch (Exception e){
-            System.out.println("Error parsing date");
-            return 0;
-        }  
+
+        return Timestamp.valueOf(newdate).getTime();
     }
 }
