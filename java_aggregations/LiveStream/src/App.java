33ef965a132370e7ed3e39d18ddd6334b8c617f2
lymperop <jimlibo13@gmail.com>
2023-02-11 21:42:03 +0200

    write raw data to influxdb, trying to write aggregated data as well

--- java_aggregations/LiveStream/src/App.java
+++ java_aggregations/LiveStream/src/App.java
@@ -3,10 +3,16 @@ import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KeyValueMapper;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Printed;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.SlidingWindows;
+import org.apache.kafka.streams.kstream.Suppressed;
 import org.apache.kafka.streams.kstream.TimeWindows;
 import org.apache.kafka.streams.kstream.Window;
 import org.apache.kafka.streams.kstream.ForeachAction;
+import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import com.influxdb.client.*;
@@ -18,12 +24,16 @@ import java.time.ZoneId;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import java.sql.Timestamp;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Locale;
 import java.util.Properties;
+
+@SuppressWarnings("all")
 
 public class App {
     public static void main(String[] args) {
@@ -32,58 +42,69 @@ public class App {
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-example");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(StreamsConfig.DEFAULT_TIMESTAMP_EXTRACTOR_CLASS_CONFIG, CustomTimestampExtractor.class.getName());
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         
         InfluxDBClient client = InfluxDBClientFactory.create("http://localhost:8086", 
-        "URcO91Nga4QLixFaMMwVpFpeExDZ93Kt54jN51VxhD_iuhxawWuJZUSNjoWcztGHuZInyqG-nL5Ev94VD_L5uw==".toCharArray(),
+        "wMqzuEl-JogXj1IrnQlwUDy9Gbv_15yvm5OQ8ua-qRvXAFdjEuJMEJ8rBEx2BqDQ6uoZ09ZfZNamhuZOrbpB6g==".toCharArray(),
         "info_sys",
-        "info_sys"
+        "info_sys_bucket"
         );
 
-        WriteApi writeApi = client.makeWriteApi(WriteOptions.builder().flushInterval(5_000).build());
+        WriteApi writeApi = client.makeWriteApi();
         
-
-        
-        System.out.println("Runnning");
         StreamsBuilder builder = new StreamsBuilder();
-        System.out.println("Running2");
         final Serde<String> stringSerde = Serdes.String();
         final Serde<Long> longSerde = Serdes.Long();
         // define the input stream and subscribe to it
-        KStream<String, String> inputStream = builder.stream("TH1", Consumed.with(stringSerde, stringSerde));
-        //inputStream.groupByKey().windowedBy(
-            //SlidingWindows.ofTimeDifferenceAndGrace(Duration.ofSeconds(96), Duration.ofSeconds(192)));
-        inputStream.peek((key, value) -> {
-            System.out.println("Received message: key = " + key + ", value = " + value);
-            String[] parts = value.split("\\|");
-            System.out.println("Before timeparsing");
-            // Instant time_parse = LocalDateTime.parse(
-            //     parts[0] ,       
-            //     DateTimeFormatter.ofPattern("YYYY-MM-dd HH:mm")  
-            // )                                     
-            // .atZone(ZoneId.of("America/Toronto"))                                     
-            // .toInstant();
-            SimpleDateFormat dateFormat = new SimpleDateFormat("YYYY-MM-dd HH:mm");
-            Date date = null;
-            try {
-                date = dateFormat.parse(parts[0]);
-            }
-            catch (Exception e) {
-                System.out.println("Error parsing date");
-            }
-            Instant time_parse = Instant.ofEpochMilli(date.getTime());
-            System.out.println("AFter time parsing");
-            Point point = Point.measurement("TH1").addField("value",Float.parseFloat(parts[1])).time(Instant.now(),WritePrecision.MS);
-            writeApi.writePoint("info_sys", "info_sys", point);         
-            });
-        System.out.println("Running3");
-
+        KStream<String, String> inputStreamTH1 = builder.stream("TH1", Consumed.with(stringSerde, stringSerde).withTimestampExtractor(new CustomTimestampExtractor()));
+        // inputStreamTH1.groupByKey().;
+        inputStreamTH1.mapValues(v -> v.split("\\|")[1]).peek((k,v)->{
+            System.out.println("Received message: key = " + k + " value = " + v);
+        }).groupByKey(Grouped.with(Serdes.String(), Serdes.String())).windowedBy(
+            SlidingWindows.ofTimeDifferenceAndGrace(Duration.ofDays(1), Duration.ofSeconds(5)))
+            .aggregate(
+            () ->  0.0D,
+            (key, value, av) -> av + Double.parseDouble(value),
+            Materialized.with(Serdes.String(), Serdes.Double())
+            )
+            .suppress(
+                Suppressed.untilWindowCloses(Suppressed.BufferConfig.unbounded())).toStream().map(
+                    (k,v) -> KeyValue.pair(k.key().toString(), v.toString())).peek((k,v) -> {
+                        System.out.println("Aggregated key=" + k + ", and aggregated value=" + v);
+                    });
 
         KafkaStreams streams = new KafkaStreams(builder.build(), props);
-        System.out.println("Running5");
         streams.start();
-        System.out.println("Running6");
 
         // shutdown hook to correctly close the streams application
         //Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+    }
+
+    public static Double adder(Double a,String b){
+        Double x = 0.0;
+        try{
+            x = Double.parseDouble(b);
+        }
+        catch(Exception e){
+            System.out.println("Error occured during parsing " + b + " to double");
+        }
+        return a+x;
+    }
+
+    public static void peek_message(String key, String value, String measurement,WriteApi influxapi) {
+            System.out.println("Received message: key = " + key + ", value = " + value);
+            String[] parts = value.split("\\|");
+            
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm", Locale.ENGLISH);
+            LocalDateTime date = null;
+            try {
+                date = LocalDateTime.parse(parts[0], dtf);
+            } catch (DateTimeParseException e) {
+                System.out.print("Error occured during date parsing");
+            }
+            
+            influxapi.writeRecord("info_sys_bucket", "info_sys", WritePrecision.MS, measurement+" value="+parts[1]+" "+Timestamp.valueOf(date).getTime());
+
     }
 }
