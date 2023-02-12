297d69e0aa456d44f22b8e2195b82d233dacb9a7
lymperop <jimlibo13@gmail.com>
2023-02-12 16:30:44 +0200

    added script to automatically stop all services

---
+++ stop_services.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# color codes for better output formatting
+GREEN='\033[0;32m'  # green color
+NC='\033[0m' # No Color
+
+# stop all services by shuting down their docker containers
+echo -e "${GREEN}Shuting down services...&{NC}"
+docker stop influxdb kafka zookeeper
+
+echo -e "${GREEN}Success!${NC}"
