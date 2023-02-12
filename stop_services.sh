416d14a2292fec50b83506830e749877fafa7711
lymperop <jimlibo13@gmail.com>
2023-02-12 19:06:19 +0200

    fixed minor bug at coloring echo output

--- stop_services.sh
+++ stop_services.sh
@@ -5,7 +5,7 @@ GREEN='\033[0;32m'  # green color
 NC='\033[0m' # No Color
 
 # stop all services by shuting down their docker containers
-echo -e "${GREEN}Shuting down services...&{NC}"
+echo -e "${GREEN}Shuting down services...${NC}"
 docker stop influxdb kafka zookeeper
 
 echo -e "${GREEN}Success!${NC}"
