6624c870da150ad51d80d6ab754b29f30741ebad
lymperop <jimlibo13@gmail.com>
2023-02-24 18:17:52 +0200

    made script able to run from any location

--- start_services.sh
+++ start_services.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+PROJECT_DIR=/home/jimlibo/git_repos/Info-systems-project-ntua/   # change with the location of the repo in your computer
+
+# go to the project directory
+cd ${PROJECT_DIR}
+
 # Start kafka and zookeeper containers
 cd setup/kafka_setup
 ./setup_kafka.sh
@@ -11,6 +16,3 @@ cd ../influxdb_setup
 # start telegraf - TODO: maybe start it manually and not from here
 cd ../telegraf_setup
 #telegraf --config telegraf.conf
-
-# return to the original directory
-cd ../../
