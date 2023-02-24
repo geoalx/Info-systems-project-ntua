435d3476920dbbd56ca0fd81cd9fe094ff474d6e
lymperop <jimlibo13@gmail.com>
2023-02-24 17:54:34 +0200

    adding code to handle failure on kafka container startup

--- setup/kafka_setup/setup_kafka.sh
+++ setup/kafka_setup/setup_kafka.sh
@@ -13,6 +13,7 @@ while [ "$1" ]; do
 
 # color codes for better terminal output
 GREEN='\033[0;32m'  # green color
+RED='\033[0;31m'  # red color
 NC='\033[0m' # No Color
 
 # create docker containers that will be running in the background
@@ -35,6 +36,29 @@ docker exec -it kafka kafka-topics.sh --create --if-not-exists --bootstrap-serve
 docker exec -it kafka kafka-topics.sh --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic W1
 docker exec -it kafka kafka-topics.sh --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic Wtot
 docker exec -it kafka kafka-topics.sh --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic Mov1
+
+# check if kafka container started, else restart the container
+if [ ! "$(docker ps | grep kafka)" ]; then
+    echo -e "${RED}Kafka container did not start correctly.Retrying...${NC}";
+    
+    # restart the kafka container
+    echo -e "${GREEN}Starting kafka container...${NC}"
+    docker start kafka
+
+    # retry to create the necessary topics
+    echo -e "${GREEN}Creating Topics in Kafka Container...${NC}"
+    docker exec -it kafka kafka-topics.sh --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic TH1
+    docker exec -it kafka kafka-topics.sh --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic TH2
+    docker exec -it kafka kafka-topics.sh --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic HVAC1
+    docker exec -it kafka kafka-topics.sh --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic HVAC2
+    docker exec -it kafka kafka-topics.sh --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MiAC1
+    docker exec -it kafka kafka-topics.sh --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MiAC2
+    docker exec -it kafka kafka-topics.sh --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic Etot
+    docker exec -it kafka kafka-topics.sh --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic W1
+    docker exec -it kafka kafka-topics.sh --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic Wtot
+    docker exec -it kafka kafka-topics.sh --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic Mov1
+
+fi
 
 # docker clean-up
 if "$cleanup"; then
